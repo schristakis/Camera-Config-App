685adad3702cc5a2d7a4f838b5d6aed2fd9c718e
SXRISTAKIS <socratisxristakis@gmail.com>
2021-04-15 12:32:20 +0300

    Deletion

--- src/Pages/General_Settings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function General_Settings() {
-    return (
-         <div className="General_Settings">
-            <h1>General settings for your camera</h1>
-         </div>
-    )
-}
