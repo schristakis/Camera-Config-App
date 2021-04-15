ace3197c5906c108367e6f54ed172839629c0357
SXRISTAKIS <socratisxristakis@gmail.com>
2021-04-15 12:28:17 +0300

    App files

---
+++ AppFiles/src/Pages/General_Settings.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function General_Settings() {
+    return (
+         <div className="General_Settings">
+            <h1>General settings for your camera</h1>
+         </div>
+    )
+}
